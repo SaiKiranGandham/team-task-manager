7dac79880b51a3ac42d8429bdb05491562a19665
Sai Kiran Gandham <165504842+SaiKiranGandham@users.noreply.github.com>
2026-05-07 10:57:03 +0530

    Frontend

---
+++ App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{padding:"40px"}}>
+      <h1>Team Task Manager</h1>
+      <p>Frontend Running Successfully</p>
+    </div>
+  );
+}
+
+export default App;
